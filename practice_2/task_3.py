c96a7b051a83a175077401c79d373022de7882be
Anton Ustinov <ustinov.ant@gmail.com>
2024-09-15 22:38:16 +0200

    homework 1 and 2

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -3,9 +3,21 @@
 Write a program to calculate the body mass index (BMI) of a person. The user must enter their height and weight, after which you use one of the formulas below to determine the index.
 BMI = weight/height**2 
 """
-weight = input()
-height = input()
+def get_positive_float_input(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+            if value <= 0:
+                raise ValueError
+            return value
+        except ValueError:
+            print("Invalid input. Please enter a correct positive real number.")
 
 
-#code here
+if __name__ == '__main__':
+    weight = get_positive_float_input("Weight (kg):")
+    height = get_positive_float_input("Height (cm):")
 
+    height = height / 100
+    bmi = weight / height ** 2
+    print(f"BMI: {bmi:.2f}")
--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -4,3 +4,23 @@ Develop a program that asks the user for an integer 4-digit number and calculate
 3 + 1 + 4 + 1 = 9
 
 """
+
+
+def get_valid_input():
+    while True:
+        try:
+            value = int(input("Enter 4-digit integer:"))
+            if value < 1000 or value > 9999:
+                raise ValueError
+            return value
+        except ValueError:
+            print("Invalid input. Please enter a 4-digit integer number.")
+
+
+if __name__ == '__main__':
+    number = get_valid_input()
+    t, rest = divmod(number, 10**3)
+    h, rest = divmod(rest, 10**2)
+    d, e = divmod(rest, 10)
+    s = t + h + d + e
+    print(f"{t} + {h} + {d} + {e} = {s}")
